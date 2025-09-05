7c25ad36bc0a43f716d2facd983fbcaf42c4db86
Nick Galbreath <client9@users.noreply.github.com>
2025-09-05 11:28:30 -0700

    cleanup logic

--- builtins/TrueQ.go
+++ builtins/TrueQ.go
@@ -2,19 +2,14 @@ package builtins
 
 import (
 	"github.com/client9/cardinal/core"
+	"github.com/client9/cardinal/core/symbol"
 	"github.com/client9/cardinal/engine"
 )
 
 // @ExprSymbol TrueQ
-// TODO: Replace with direct atom comparison
 
 // TrueQ check is an expression is explicity True
 // @ExprPattern (_)
 func TrueQ(e *engine.Evaluator, c *engine.Context, args []core.Expr) core.Expr {
-	expr := args[0]
-	// Check new Symbol type first
-	if symbolName, ok := core.ExtractSymbol(expr); ok {
-		return core.NewBool(symbolName == "True")
-	}
-	return core.NewBool(false)
+	return core.NewBool(args[0] == symbol.True)
 }
